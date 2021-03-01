0504c2b2fe4c80aad67e1aedebbd9fb1b12a1da3
Genaro Coronel <genaro.coronel561@gmail.com>
2021-03-01 09:50:44 +0100

    set up typescript and OVM compilation

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,10 +1,15 @@
-require("@nomiclabs/hardhat-waffle");
-require("hardhat-gas-reporter");
+import { task } from "hardhat/config";
+import { HardhatUserConfig } from "hardhat/config";
+
+import "@nomiclabs/hardhat-waffle";
+import "hardhat-gas-reporter";
+import "@eth-optimism/plugins/hardhat/compiler";
+import "@eth-optimism/plugins/hardhat/ethers";
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async () => {
-  const accounts = await ethers.getSigners();
+task("accounts", "Prints the list of accounts", async (args, hre) => {
+  const accounts = await hre.ethers.getSigners();
 
   for (const account of accounts) {
     console.log(account.address);
@@ -14,10 +19,7 @@ task("accounts", "Prints the list of accounts", async () => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
-module.exports = {
+const config: HardhatUserConfig = {
   solidity: {
     version: "0.7.3",
     settings: { optimizer: { enabled: true, runs: 1000 } },
@@ -27,3 +29,5 @@ module.exports = {
     gasPrice: 100,
   },
 };
+
+export default config;
---
+++ test/optimistic-greeter.spec.ts
@@ -0,0 +1,15 @@
+const { expect } = require("chai");
+import { l2ethers as ethers } from "hardhat";
+
+describe("Greeter on OVM", function () {
+  it("Should return the new greeting once it's changed", async function () {
+    const Greeter = await ethers.getContractFactory("Greeter");
+    const greeter = await Greeter.deploy("Hello, world!");
+
+    await greeter.deployed();
+    expect(await greeter.greet()).to.equal("Hello, world!");
+
+    await greeter.setGreeting("Hola, mundo!");
+    expect(await greeter.greet()).to.equal("Hola, mundo!");
+  });
+});
